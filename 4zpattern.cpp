d6bed7d6091b4adb4d698b79e83257dc2b7e4fb1
deepanshu-QM <cdeepanshu231@gmail.com>
2026-01-18 00:22:39 +0530

    Second Commit

---
+++ 4zpattern.cpp
@@ -0,0 +1,21 @@
+/*
+print the following pattern exactly as shown:
+No Input
+*****
+   *
+  *
+ *
+*****          */
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout<<"*****"<<endl;
+    cout<<"   * "<<endl;
+    cout<<"  *  "<<endl;
+    cout<<" *   "<<endl;
+    cout<<"*****"<<endl;
+
+    return 0;
+}
