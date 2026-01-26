317f2d60070d987b9c2e4bbed7c1a13760f4c492
deepanshu-QM <cdeepanshu231@gmail.com>
2026-01-26 15:48:43 +0530

    Changes

---
+++ 24loop.cpp
@@ -0,0 +1,15 @@
+
+//https://codeforces.com/group/4vcXCPx8NY/contest/666169/problem/H
+
+#include<iostream>
+using namespace std;
+
+
+int main(){
+    int n;
+    cin>>n;
+
+    long long sum = (n*(n+1))/2;
+    cout<<sum<<endl;
+}
+
