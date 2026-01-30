4399f7ec17f04413441d082cf94aef747e2faf9b
deepanshu-QM <cdeepanshu231@gmail.com>
2026-01-31 02:53:31 +0530

    hollow Vertical Triangle

---
+++ 2patterprinting/11HollowVerTri.cpp
@@ -0,0 +1,29 @@
+
+//https://codeforces.com/group/4vcXCPx8NY/contest/666171/problem/K
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            if(i == 1 || j == 1 || j == i){
+                cout<<"* ";
+            }else{
+                cout<<" ";
+            }
+        }cout<<endl;
+    }
+    for(int i=n-1;i>0;i--){
+        for(int j=1;j<=i;j++){
+            if(i ==1 || j ==1 || j== i){
+                cout<<"* ";
+            }else{
+                cout<<" ";
+            }
+        }cout<<endl;
+    }
+    return 0;
+}
---
+++ 2patterprinting/12Triagle.cpp
@@ -0,0 +1,8 @@
+
+//https://codeforces.com/group/4vcXCPx8NY/contest/666171/problem/L
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+}
