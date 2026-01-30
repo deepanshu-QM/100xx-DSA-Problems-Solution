84ab5e0d72d1183080c77595d3718a3184404dee
deepanshu-QM <cdeepanshu231@gmail.com>
2026-01-30 14:41:57 +0530

    pattern assignment

---
+++ 2patterprinting/1pattern.cpp
@@ -0,0 +1,17 @@
+
+//https://codeforces.com/group/4vcXCPx8NY/contest/666171/problem/A
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=2;j++){
+            cout<<"*";
+        }cout<<endl;
+    }
+    return 0;
+}
