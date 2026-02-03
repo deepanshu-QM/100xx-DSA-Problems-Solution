8b5221639bd7db3a0ac5ca3b5baf4c76adc96ce6
deepanshu-QM <cdeepanshu231@gmail.com>
2026-02-03 23:30:55 +0530

    Function assignement

---
+++ function/1hello.cpp
@@ -0,0 +1,18 @@
+
+//https://codeforces.com/group/4vcXCPx8NY/contest/668068/problem/A
+
+#include<iostream>
+using namespace std;
+
+void printLines(int N){
+    for(int i=1;i<=N;i++){
+        cout<<"I am learning functions"<<endl;
+    }
+}
+int main(){
+    int N;
+    cin>>N;
+    printLines(N);
+
+    return 0;
+}
