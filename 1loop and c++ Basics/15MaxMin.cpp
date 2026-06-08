4f6628a1d392e0a9af450fe8f36de1b5f4896e02
deepanshu-QM <cdeepanshu231@gmail.com>
2026-06-09 00:06:33 +0530

    Max and Min

--- 1loop and c++ Basics/15MaxMin.cpp
+++ 1loop and c++ Basics/15MaxMin.cpp
@@ -1,3 +1,17 @@
 
 // https://codeforces.com/group/4vcXCPx8NY/contest/664504/problem/N
 
+
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int main(){
+    long long A,B;
+    cin>>A>>B;
+
+    cout<<"Min = "<<min(A,B)<<endl;
+    cout<<"Max = "<<max(A,B)<<endl;
+
+    return 0;
+}
