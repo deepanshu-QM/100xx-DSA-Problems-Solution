8667b3bb09cc0dad7ed33862ccfdc47346fe0d6c
deepanshu-QM <cdeepanshu231@gmail.com>
2026-06-08 23:58:56 +0530

    Temp Folder

---
+++ practice/main.cpp
@@ -0,0 +1,31 @@
+
+
+/*
+Origin — if X=0
+ and Y=0
+X axis — if Y=0
+ and X≠0
+Y axis — if X=0
+ and Y≠0
+1st Quadrant — if X>0
+ and Y>0
+2nd Quadrant — if X<0
+ and Y>0
+3rd Quadrant — if X<0
+ and Y<0
+4th Quadrant — if X>0
+ and Y<0*/
+
+ #include<iostream>
+ using namespace std;
+
+ int main(){
+    long long X,Y;
+    cin>>X>>Y;
+
+    if(X = 0 && Y = 0){
+        cout<<"Origin"<<endl;
+    }else if(X != 0 && Y = 0){
+        cout<<"X axis "
+    }
+ }
