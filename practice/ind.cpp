003138939186ba1d98b7c841fb645d372b0fefac
deepanshu-QM <cdeepanshu231@gmail.com>
2026-06-11 23:03:45 +0530

    Changes

---
+++ practice/ind.cpp
@@ -0,0 +1,11 @@
+
+#include<iostream>
+using  namespace std;
+int main(){
+    long long n,sum=0;
+    cin>>n;
+    while(n>0){
+        sum += n;
+    }
+    cout<<sum;
+}
--- practice/main.cpp
+++ practice/main.cpp
@@ -1,29 +1,15 @@
 
-
 #include<iostream>
 using namespace std;
 
 int main(){
-    int n, pos = 0, neg = 0, even = 0, odd = 0;      
+    long long n,temp=0;
     cin>>n;
 
-    long long N[n];
-    for(int i=1;i<=n;i++){
-        cout<<"Enter Elements : "<<endl;
-        cin>>N[i];
-
-        if(N[i] >= 0) pos++;
-
-        else  neg++;
-        
-        if(N[i] % 2 == 0) even++;
-        else odd ++;
-    } 
-    cout<<pos<<endl;
-    cout<<neg<<endl;
-    cout<<even<<endl;
-    cout<<odd<<endl;
-
+    while(n>0){
+        temp = temp * 10 + (n%10);
+        n = n/10;
+    }
+    cout<<temp<<endl;
     return 0;
-
 }
