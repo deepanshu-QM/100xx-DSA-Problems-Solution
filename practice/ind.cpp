394810b311d2f59536753f0d3f209e63e4f4dd88
deepanshu-QM <cdeepanshu231@gmail.com>
2026-06-13 22:46:47 +0530

    Greedy algo

--- practice/ind.cpp
+++ practice/ind.cpp
@@ -1,11 +1,15 @@
 
-#include<iostream>
-using  namespace std;
-int main(){
-    long long n,sum=0;
-    cin>>n;
-    while(n>0){
-        sum += n;
+#include <iostream>
+using namespace std;
+
+int main() {
+    int amount = 289;
+    int coins[] = {100, 50, 20, 10, 5, 1};
+
+    for(int i = 0; i < 6; i++) {
+        while(amount >= coins[i]) {
+            amount -= coins[i];
+            cout << coins[i] << " ";
+        }
     }
-    cout<<sum;
 }
